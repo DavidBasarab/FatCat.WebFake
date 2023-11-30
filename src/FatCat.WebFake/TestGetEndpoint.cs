2180fd3f5ccb6ce863f3ff6c1d235912fc12c71f
David Basarab <dbasarab617@gmail.com>
2023-11-29 22:35:35 -0500

    now can get any get

--- src/FatCat.WebFake/TestGetEndpoint.cs
+++ src/FatCat.WebFake/TestGetEndpoint.cs
@@ -1,15 +1,18 @@
 ﻿using FatCat.Toolkit.Console;
 using FatCat.Toolkit.WebServer;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FatCat.WebFake;
 
 public class TestGetEndpoint : Endpoint
 {
-    [HttpGet("test")]
+    [HttpGet("{*url}")]
     public WebResult TestGet()
     {
-        ConsoleLog.WriteCyan("Test Get Endpoint");
+        var displayUrl = Request.GetDisplayUrl();
+
+        ConsoleLog.WriteCyan($"Test Get Endpoint from | <{displayUrl}>");
 
         return WebResult.Ok($"ACK from Test Get Endpoint | {DateTime.Now:h:mm:ss tt}");
     }
