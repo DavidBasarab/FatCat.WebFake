e5d597b39d345e13894befe260dde332d3abb40e
DBasarab <dbasarab617@gmail.com>
2023-11-30 20:30:38 -0500

    proof of concept

--- src/FatCat.WebFake/CatchAllGetEndpoint.cs
+++ src/FatCat.WebFake/CatchAllGetEndpoint.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace FatCat.WebFake;
 
-public class CatchAllGetEndpoint(IConfiguration configuration) : Endpoint
+public class CatchAllGetEndpoint(IWebFakeSettings settings) : Endpoint
 {
     [HttpGet("{*url}")]
     public WebResult TestGet()
@@ -14,7 +14,7 @@ public class CatchAllGetEndpoint(IConfiguration configuration) : Endpoint
         var displayUrl = Request.GetDisplayUrl();
 
         ConsoleLog.WriteCyan($"Test Get Endpoint from | <{displayUrl}>");
-        ConsoleLog.WriteMagenta($"Configuration testing | configuration[\"david\"] := <{configuration["david"]}>");
+        ConsoleLog.WriteMagenta($"Configuration testing | settings.UniqueId := <{settings.UniqueId}>");
 
         return WebResult.Ok($"ACK from Test Get Endpoint | {DateTime.Now:h:mm:ss tt}");
     }
---
+++ src/FatCat.WebFake/FakeUniqueId.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FatCat.WebFake;
+
+public interface IWebFakeSettings
+{
+	string UniqueId { get; }
+}
+
+public class WebFakeSettings(IConfiguration configuration) : IWebFakeSettings
+{
+	public string UniqueId
+	{
+		get => configuration["FakeId"];
+	}
+}
