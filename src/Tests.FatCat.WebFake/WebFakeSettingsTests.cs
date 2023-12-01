7d6f8a8f31bee8afdb8358665ffa3d24ab32f210
DBasarab <dbasarab617@gmail.com>
2023-11-30 20:35:08 -0500

    setting fake id

---
+++ src/Tests.FatCat.WebFake/WebFakeSettingsTests.cs
@@ -0,0 +1,36 @@
+﻿using FakeItEasy;
+using FatCat.Fakes;
+using FatCat.WebFake;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Tests.FatCat.WebFake;
+
+public class WebFakeSettingsTests
+{
+	private readonly IConfiguration configuration = A.Fake<IConfiguration>();
+	private readonly string FakeId = Faker.RandomString();
+	private readonly WebFakeSettings webSettings;
+
+	public WebFakeSettingsTests()
+	{
+		A.CallTo(() => configuration["FakeId"]).ReturnsLazily(() => FakeId);
+
+		webSettings = new WebFakeSettings(configuration);
+	}
+
+	[Fact]
+	public void GetFakeId()
+	{
+		var _ = webSettings.UniqueId;
+
+		A.CallTo(() => configuration["FakeId"]).MustHaveHappened();
+	}
+
+	[Fact]
+	public void ReturnFakeId()
+	{
+		webSettings.UniqueId.Should().Be(FakeId);
+	}
+}
