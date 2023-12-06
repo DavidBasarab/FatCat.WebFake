d51922cc83b489c856e34b10373699d4dc8d8f73
DBasarab <dbasarab617@gmail.com>
2023-12-06 10:26:11 -0500

    basic post is working

--- src/WebFake/ServiceModels/EntryRequest.cs
+++ src/WebFake/ServiceModels/EntryRequest.cs
@@ -1,12 +1,15 @@
 ﻿using FatCat.Toolkit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace FatCat.WebFake.ServiceModels;
 
 public class EntryRequest : EqualObject
 {
-	public HttpVerb Verb { get; set; }
-
 	public string Path { get; set; }
 
 	public EntryResponse Response { get; set; }
+
+	[JsonConverter(typeof(StringEnumConverter))]
+	public HttpVerb Verb { get; set; }
 }
