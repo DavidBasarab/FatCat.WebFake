7dfcab7a968d511a4583350b7e17993f4f8277aa
David Basarab <dbasarab617@gmail.com>
2023-12-10 21:41:54 -0500

    got container pulled from hub

--- src/Scripts/BuildDockerImage.ps1
+++ src/Scripts/BuildDockerImage.ps1
@@ -2,12 +2,12 @@ $webFakeFolderItem = Get-Item "..\"
 $dockerFileItem = Get-Item -Path "..\WebFake\Dockerfile"
 
 $dockerfilePath = $dockerFileItem.FullName
-$imageName = "web-fake"
+$imageName = "dbasarab/web-fake:latest"
 $webFakeFolderPath = $webFakeFolderItem.FullName
-
 
 Write-Host "Docker file path: $dockerfilePath"
 Write-Host "Image name: $imageName"
 Write-Host "Web fake folder path: $webFakeFolderPath"
 
 docker build -t $imageName -f $dockerfilePath $webFakeFolderItem.FullName
+docker push $imageName
