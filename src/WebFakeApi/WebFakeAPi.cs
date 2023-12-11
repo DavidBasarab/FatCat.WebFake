c836e5ea036f591a7e668c1e7b26db18171dfaa3
David Basarab <dbasarab617@gmail.com>
2023-12-10 22:14:53 -0500

    defined basic interface

---
+++ src/WebFakeApi/WebFakeAPi.cs
@@ -0,0 +1,46 @@
+﻿using FatCat.Toolkit.Web;
+using FatCat.WebFakeApi.Models;
+
+namespace FatCat.WebFakeApi;
+
+public interface IWebFakeAPi
+{
+	string FakeId { get; }
+
+	Uri FakeUri { get; }
+
+	Task<FatWebResponse> CreateResponse(EntryResponse response);
+
+	Task<FatWebResponse> DeleteResponse(string pathToDelete);
+
+	Task<FatWebResponse<ClientRequest>> GetAllClientRequests();
+
+	Task<FatWebResponse<EntryRequest>> GetAllResponses();
+}
+
+public class WebFakeAPi(Uri fakeUri, string fakeId) : IWebFakeAPi
+{
+	public string FakeId { get; } = fakeId;
+
+	public Uri FakeUri { get; } = fakeUri;
+
+	public Task<FatWebResponse> CreateResponse(EntryResponse response)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<FatWebResponse> DeleteResponse(string pathToDelete)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<FatWebResponse<ClientRequest>> GetAllClientRequests()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<FatWebResponse<EntryRequest>> GetAllResponses()
+	{
+		throw new NotImplementedException();
+	}
+}
